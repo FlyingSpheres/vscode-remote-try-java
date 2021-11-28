795da2746a2e81fa653aa4a1a478a25043a18f0c
Aaron <aaron@flyingspheres.com>
2021-11-28 21:32:15 +0000

    new branch to configure Liberty

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,10 +5,22 @@
 
 package com.mycompany.app;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello Remote World!" );
+        long startTime = System.currentTimeMillis();
+        try{
+            Thread.sleep(2500);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime-startTime)/1000.0);
+
     }
 }
