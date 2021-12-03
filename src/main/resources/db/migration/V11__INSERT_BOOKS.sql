06b9510cdd915c9068567dd90aa6e6f4ea23029f
Aaron <aaron@flyingspheres.com>
2021-12-03 15:40:00 +0000

    Added the insert statements to the database

---
+++ src/main/resources/db/migration/V11__INSERT_BOOKS.sql
@@ -0,0 +1,5 @@
+INSERT INTO `books` (`id`, `name`, `author`, `language`, `image`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Harry Potter y la piedra filosofal', 'J. K. Rowling', 'ES', 'none', NULL, NULL, NULL),
+(3, 'Harry Potter y la cámara de los secretos', 'J. K. Rowling', 'ES', 'none', NULL, NULL, NULL),
+(4, 'Harry Potter y el prisionero de azkaban', 'J. K. Rowling', 'ES', 'none', NULL, NULL, NULL),
+(5, 'El ojo del mundo', 'Robert Jordan', 'ES', 'none', NULL, NULL, NULL);
